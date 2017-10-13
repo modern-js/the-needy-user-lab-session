330b26c3fa96459129d4d224d74af7123cba2a14
SPAWNS <P2DUCHEV@ABV.bg>
2017-10-13 16:54:58 +0300

    First commit

--- client/api.js
+++ client/api.js
@@ -2,14 +2,33 @@
 if (typeof global === 'undefined') {
   global = window;
 }
-
 /* write your code here */
 
+const get = something => {
+  return global.fetch(`/${something}`);
+};
+
+get.me = something => {
+  return global.fetch(`/${something}`);
+};
+
+get.me.everything = () => {
+  const {food, drink, tv} = {food: global.fetch(`/food`),
+    drink: global.fetch(`/drink`),
+    tv: global.fetch(`/tv`)};
+  return Promise.all([food, drink, tv]);
+};
+
+
+get.me.a = something => {
+  return global.fetch(`/${something}`);
+};
 
 
 // define the api-client object to be exported
 const api = {
-  
+  get: get,
+  go: { get: get}
 };
 
 // making the code work both in the browser and node.js
--- server/api.js
+++ server/api.js
@@ -2,4 +2,37 @@ const api = require('express')();
 
 /* your code here */
 
+let totalRequests = 0;
+
+api.get('/food', (req, res) => {
+
+  if(totalRequests >= 10){
+    res.send({ error: 404 })
+  } else {
+    totalRequests++;
+    res.send({ food: "food" });
+  }
+
+});
+
+api.get('/drink', (req, res) => {
+
+  if(totalRequests >= 10){
+    res.send({ error: 404 })
+  } else {
+    totalRequests++;
+    res.send({ drink: "drink" });
+  }
+});
+
+api.get('/tv', (req, res) => {
+
+  if(totalRequests >= 10){
+    res.send({ error: 404 })
+  } else {
+    totalRequests++;
+    res.send({ tv: "tv"});
+  }
+});
+
 module.exports = api;
