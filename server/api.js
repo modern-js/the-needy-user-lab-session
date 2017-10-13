4e820b08174bea0cf298059149fdad897d98eb83
Nikolay Pohlupkov <nikolaypoh@gmail.com>
2017-10-13 16:55:04 +0300

    Solution

--- server/api.js
+++ server/api.js
@@ -1,5 +1,23 @@
 const api = require('express')();
-
+const rate_limiter = require('express-rate-limit');
 /* your code here */
 
 module.exports = api;
+
+let apiLimiter = new rate_limiter({
+  windowMs: 15*60*1000, // 15 minutes
+  max: 10,
+  messsage: { error: ''}
+});
+
+api.get('/food', apiLimiter, (req, res) => {
+  res.send({ food: 'Food' });
+});
+
+api.get('/drink', apiLimiter, (req, res) => {
+  res.send({ drink: 'Drink' });
+});
+
+api.get('/tv', apiLimiter, (req, res) => {
+  res.send({ tv: 'tv' });
+});
