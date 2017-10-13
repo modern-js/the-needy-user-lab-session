30af936359770845deaba48a4537b36c26678d14
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2017-10-13 16:46:50 +0300

    Submission

--- client/api.js
+++ client/api.js
@@ -8,8 +8,47 @@ if (typeof global === 'undefined') {
 
 
 // define the api-client object to be exported
-const api = {
-  
+const api = {};
+
+api.get = (something) => {
+  return global.fetch(`/${something}`);
+};
+
+api.get.me = (something) => {
+  return global.fetch(`/${something}`);
+};
+
+api.get.me.a = (something) => {
+  return global.fetch(`/${something}`);
+};
+
+
+api.get.me.everything = (something) => {
+  const {food, drink, tv} = {food: global.fetch(`/food`),
+                             drink: global.fetch(`/drink`),
+                             tv: global.fetch(`/tv`)};
+  return Promise.all([food, drink, tv]);
+};
+
+api.go = {};
+
+api.go.get = (something) => {
+  return global.fetch(`/${something}`);
+};
+
+api.go.get.me = (something) => {
+  return global.fetch(`/${something}`);
+};
+
+api.go.get.me.a = (something) => {
+  return global.fetch(`/${something}`);
+};
+
+api.go.get.me.everything = () => {
+  const {food, drink, tv} = {food: global.fetch(`/food`),
+                             drink: global.fetch(`/drink`),
+                             tv: global.fetch(`/tv`)};
+  return Promise.all([food, drink, tv]);
 };
 
 // making the code work both in the browser and node.js
--- server/api.js
+++ server/api.js
@@ -2,4 +2,37 @@ const api = require('express')();
 
 /* your code here */
 
+let totalRequests = 0;
+
+api.get('/food', (req, res) => {
+
+    if(totalRequests >= 10){
+        res.send({ error: 4 })
+    } else {
+        totalRequests++;
+        res.send({ food: 1 });
+    }
+    
+});
+
+api.get('/drink', (req, res) => {
+    
+    if(totalRequests >= 10){
+        res.send({ error: 4 })
+    } else {
+        totalRequests++;
+        res.send({ drink: 2 });
+    }
+});
+
+api.get('/tv', (req, res) => {
+    
+    if(totalRequests >= 10){
+        res.send({ error: 4 })
+    } else {
+        totalRequests++;
+        res.send({ tv: 3 });
+    }
+});
+
 module.exports = api;
