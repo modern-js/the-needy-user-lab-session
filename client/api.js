cf31a251bd3f5cbf8e6fc3220cfd81fb5fb7d4bc
Edor Kacerja <hrist0.stoichev@mail.bg>
2017-10-13 16:59:02 +0300

    changes

--- client/api.js
+++ client/api.js
@@ -5,11 +5,32 @@ if (typeof global === 'undefined') {
 
 /* write your code here */
 
+const get = (parameter) => {
+  return global.fetch("localhost:8053/tv");
+};
 
+get.me = (parameter) => {
+  return global.fetch("localhost:8053/tv");
+};
+
+get.me.a = (parameter) => {
+  return global.fetch("localhost:8053/tv");
+};
+
+get.me.everything = () => {};
+
+const go = () => {};
+
+go.get = get;
+
+go.get.me.everything = () => {
+  return Promise.all([global.fetch("localhost:8053/tv"), global.fetch("localhost:8053/drink"), global.fetch("localhost:8053/food")]);
+};
 
 // define the api-client object to be exported
 const api = {
-  
+  get,
+  go
 };
 
 // making the code work both in the browser and node.js
--- server/api.js
+++ server/api.js
@@ -1,5 +1,27 @@
 const api = require('express')();
 
-/* your code here */
+let requestsCount = 0;
+
+api.use((req, res, next) => {
+    requestsCount += 1;
+
+    if (requestsCount > 10) {
+        res.send({error: 'You cannot make more than 10 requests!'});
+    } else {
+        next();
+    }
+});
+
+api.get('/food', (req, res) => {
+    res.send({food: 'banana'});
+});
+
+api.get('/drink', (req, res) => {
+    res.send({drink: 'soda'});
+});
+
+api.get('/tv', (req, res) => {
+    res.send({tv: 'btv'});
+});
 
 module.exports = api;
