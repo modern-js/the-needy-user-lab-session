999aa5c606db4ffc32193e114073ca52dd5027cd
Nikolay Pohlupkov <nikolaypoh@gmail.com>
2017-10-13 17:03:15 +0300

    removed rate-limiter, did it with counter

--- server/api.js
+++ server/api.js
@@ -1,23 +1,43 @@
 const api = require('express')();
-const rate_limiter = require('express-rate-limit');
+//const rate_limiter = require('express-rate-limit');
 /* your code here */
 
 module.exports = api;
 
-let apiLimiter = new rate_limiter({
-  windowMs: 15*60*1000, // 15 minutes
-  max: 10,
-  messsage: { error: ''}
+
+// Rate limiter should return an error property, I don't know why it didnt' work
+
+// let apiLimiter = new rate_limiter({
+//   windowMs: 15*60*1000, // 15 minutes
+//   max: 10,
+//   messsage: { error: ''}
+// });
+
+let maxRequest = 0;
+
+api.get('/food', (req, res) => {
+  if (maxRequest >= 10) {
+    res.send({ error: 'error' })
+  } else {
+    maxRequest++;
+    res.send({ food: 'Food' });
+  }
 });
 
-api.get('/food', apiLimiter, (req, res) => {
-  res.send({ food: 'Food' });
+api.get('/drink', (req, res) => {
+  if (maxRequest >= 10) {
+    res.send({ error: 'error' })
+  } else {
+    maxRequest++;
+    res.send({ drink: 'Drink' });
+  }
 });
 
-api.get('/drink', apiLimiter, (req, res) => {
-  res.send({ drink: 'Drink' });
-});
-
-api.get('/tv', apiLimiter, (req, res) => {
-  res.send({ tv: 'tv' });
+api.get('/tv', (req, res) => {
+  if (maxRequest >= 10) {
+    res.send({ error: 'error' })
+  } else {
+    maxRequest++;
+    res.send({ tv: 'tv' });
+  }
 });
